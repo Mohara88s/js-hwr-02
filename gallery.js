761c33336de8392690ff4bfdec8916b19cd1b6eb
Mohara88s <73835861+Mohara88s@users.noreply.github.com>
2021-04-15 15:21:39 +0300

    hw done with additional tasks

--- gallery.js
+++ gallery.js
@@ -6,14 +6,19 @@
 // Очистка значения атрибута src элемента img.lightbox__image.Это необходимо для того, чтобы
 // при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
 
+// Дополнительно
+// Закрытие модального окна по клику на div.lightbox__overlay.
+// Закрытие модального окна по нажатию клавиши ESC.
+// Пролистывание изображений галереи в открытом модальном окне клавишами "влево" и "вправо".
+
 import atributesSource from './gallery-items.js'
-console.log(atributesSource)
 
 const galleryContainer = document.querySelector('ul.js-gallery')
 const lightboxContainer = document.querySelector('.js-lightbox')
 const lightboxImage = document.querySelector('img.lightbox__image')
 const lightboxCloseButton = document.querySelector('button[data-action="close-lightbox"]')
 
+//creating markup
 const galleryMarkup = createImages(atributesSource)
 galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
 function createImages(images) {
@@ -35,22 +40,70 @@ function createImages(images) {
 `}).join('')
 }
 
+//none linking
 galleryContainer.addEventListener('click', stopDefAction)
 function stopDefAction(evt) {
     evt.preventDefault();
 }
 
-galleryContainer.addEventListener('click', onGalleryContainerClick)
-function onGalleryContainerClick(event) {   
-if (!event.target.classList.contains('gallery__image')) {return}
+//to open modal
+galleryContainer.addEventListener('click', onGalleryImageClick)
+function onGalleryImageClick(event) {   
+  if (!event.target.classList.contains('gallery__image')) { return }
   lightboxContainer.classList.add('is-open')
   lightboxImage.setAttribute('src', `${event.target.dataset.source}`)
   lightboxImage.setAttribute('alt', `${event.target.getAttribute('alt')}`)
+
+  window.addEventListener('keydown', onKaydown)
+  lightboxCloseButton.addEventListener('click', onlightboxCloseButtoClick)
+  lightboxContainer.addEventListener('click', onlightboxOverlayClick)
 }
 
-lightboxCloseButton.addEventListener('click', onLightboxCloseButton)
-function onLightboxCloseButton() {
+//to close modal
+function lightboxClose() {
   lightboxContainer.classList.remove('is-open')
   lightboxImage.setAttribute('src', '')
   lightboxImage.setAttribute('alt', '')
+
+  window.removeEventListener('keydown', onKaydown)
+  lightboxCloseButton.removeEventListener('click', onlightboxCloseButtoClick)
+  lightboxContainer.removeEventListener('click', onlightboxOverlayClick)
 }
+
+//onClick
+function onlightboxCloseButtoClick() {
+  lightboxClose()
+}
+function onlightboxOverlayClick(event) {
+  if (event.target.classList.contains('lightbox__overlay')) {lightboxClose()}
+}
+
+// onKaydown
+function onKaydown(event) {
+  if (event.code === 'Escape') {
+    lightboxClose()
+  }
+  if (event.code === 'ArrowRight') {
+    onArrowRightKaydown()
+  }
+  if (event.code === 'ArrowLeft') {
+    onArrowLeftKaydown()
+  }
+}
+
+//arrows
+function lightboxImageIndex() {
+  return atributesSource.indexOf(atributesSource.find(element => element.original === lightboxImage.src))
+}
+function onArrowRightKaydown() {
+  let i = lightboxImageIndex() + 1
+  if (i === atributesSource.length) { i = 0 }
+  lightboxImage.setAttribute('src', atributesSource[i].original)
+  lightboxImage.setAttribute('alt', atributesSource[i].description)
+}
+function onArrowLeftKaydown() {
+  let i = lightboxImageIndex() - 1
+  if (i === -1) { i = atributesSource.length - 1}
+  lightboxImage.setAttribute('src', atributesSource[i].original)
+  lightboxImage.setAttribute('alt', atributesSource[i].description)
+ }
